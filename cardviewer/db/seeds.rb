ca88d2eaa2c5a99719b96d7e9060949c77b2b40e
Andrew Schnieder <andrewschnieder@gmail.com>
2015-07-14 18:42:46 -0700

    fix seed file to add image url to database

--- cardviewer/db/seeds.rb
+++ cardviewer/db/seeds.rb
@@ -7,12 +7,13 @@ p database_hash[0]["name"]
 
 
 database_hash.each do |card|
+    multiverseid = card["multiverseid"].to_s
   c = Card.create(
     name: card["name"],
     multiverseid: card["multiverseid"],
     rarity: card["rarity"],
     set: "ktk",
-    image: "http://api.mtgdb.info/content/card_images/" + "card['multiverseid']" + ".jpeg"
+    image: "http://api.mtgdb.info/content/card_images/" + "#{multiverseid}" + ".jpeg"
     )
 end
 
@@ -23,12 +24,13 @@ p database_hash[0]["name"]
 
 
 database_hash.each do |card|
+    multiverseid = card["multiverseid"].to_s
   c = Card.create(
     name: card["name"],
     multiverseid: card["multiverseid"],
     rarity: card["rarity"],
     set: "frf",
-    image: "http://api.mtgdb.info/content/card_images/" + "card['multiverseid']" + ".jpeg"
+    image: "http://api.mtgdb.info/content/card_images/" + "#{multiverseid}" + ".jpeg"
     )
 end
 
@@ -39,11 +41,12 @@ p database_hash[0]["name"]
 
 
 database_hash.each do |card|
+    multiverseid = card["multiverseid"].to_s
   c = Card.create(
     name: card["name"],
     multiverseid: card["multiverseid"],
     rarity: card["rarity"],
     set: "dtk",
-    image: "http://api.mtgdb.info/content/card_images/" + "card['multiverseid']" + ".jpeg"
+    image: "http://api.mtgdb.info/content/card_images/" + "#{multiverseid}" + ".jpeg"
    )
 end
